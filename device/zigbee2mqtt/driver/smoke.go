eb8d63f3ff838310ddf02acb1cff172bd816d8b7
Markus Troßbach <markus@trossbach.net>
2022-08-31 20:04:39 +0200

    Implement smoke sensor (#21)

--- device/zigbee2mqtt/driver/smoke.go
+++ device/zigbee2mqtt/driver/smoke.go
@@ -5,19 +5,20 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/mtrossbach/waechter/device"
 	"github.com/mtrossbach/waechter/device/zigbee2mqtt/connector"
+	"github.com/mtrossbach/waechter/internal/cfg"
 	"github.com/mtrossbach/waechter/internal/log"
 	"github.com/mtrossbach/waechter/system"
 )
 
-func SirenHandler(dev *system.Device, controller device.SystemController) connector.MessageHandler {
+func SmokeSensorHandler(dev *system.Device, controller device.SystemController) connector.MessageHandler {
 	return func(msg mqtt.Message) {
-		var payload sirenStatus
+		var payload smokeStatus
 		if err := json.Unmarshal(msg.Payload(), &payload); err != nil {
 			log.Error().Str("payload", string(msg.Payload())).Msg("Could not parse payload")
 			return
 		}
 
-		log.Debug().Str("payload", string(msg.Payload())).Msg("Got data")
+		log.Debug().RawJSON("payload", msg.Payload()).Msg("Got data")
 
 		if payload.Battery > 0 {
 			controller.ReportBatteryLevel(float32(payload.Battery)/float32(100), *dev)
@@ -30,9 +31,19 @@ func SirenHandler(dev *system.Device, controller device.SystemController) connec
 		if payload.Tamper {
 			controller.Alarm(system.TamperAlarm, *dev)
 		}
+
+		if payload.Smoke {
+			controller.Alarm(system.FireAlarm, *dev)
+		}
 	}
 }
 
-func SirenStateUpdater(controller device.SystemController, sender Sender) {
-	sender(newWarningPayload(controller.GetAlarmType()))
+func SmokeStateUpdater(controller device.SystemController, sender Sender) {
+	var payload warningPayload
+	if controller.GetAlarmType() != system.NoAlarm && cfg.GetBool(cEnabled) {
+		payload = newWarningPayload(controller.GetAlarmType())
+	} else {
+		payload = newWarningPayload(system.NoAlarm)
+	}
+	sender(payload)
 }
